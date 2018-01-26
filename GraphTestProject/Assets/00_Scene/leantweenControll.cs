337b0666a356960c08acfdd5044641a76f96277c
jinmi <plus0721@gmail.com>
2018-01-26 18:20:25 +0900

    그래프 테스트하고, 린트윈으로 움직여보고, 그래프랑 NGUI 같이 나오게도 해봄

---
+++ GraphTestProject/Assets/00_Scene/leantweenControll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leantweenControll : MonoBehaviour {
+
+    public Transform MyTrans;
+
+	// Update is called once per frame
+	void Update () {
+        //LeanTween.scaleX(this.gameObject, 10f, 3f).setEase(LeanTweenType.easeInElastic);
+
+        //LeanTween.scaleY(this.gameObject, 10f, 3f).setEase(LeanTweenType.easeInExpo);
+
+        LeanTween.move(this.gameObject, MyTrans, 1f).setEase(LeanTweenType.easeOutQuart);
+	}
+}
